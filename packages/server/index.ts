f855102370e923cba43672271542de32a6ed9101
Krish90s <krishnanvenkat4@gmail.com>
2025-09-02 20:55:33 +0530

    connected front end and backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -10,7 +10,9 @@ const port = process.env.PORT || 3000;
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript Express!');
 });
-
+app.get('/api/hello', (req: Request, res: Response) => {
+  res.json({message : "Hello World!"});
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
